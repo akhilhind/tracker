6c1be7d25476f17c4c774ac50a90a0999d537c60
Akhil <33095832+akhilhind@users.noreply.github.com>
2018-09-06 23:57:30 +0530

    Add files via upload

---
+++ objSet.js
@@ -0,0 +1,44 @@
+// class newAdmin {
+//     constructor(name, email, pass) {
+//         this.name = name;
+//         this.email = email;
+//         this.pass = pass;
+//     }
+// }
+
+// class newUser {
+//     constructor(name,pass,email,dob,address) {
+//         this.name = name;
+//         this.pass = pass;
+//         this.email = email;
+//         this.dob = dob;
+//         this.add = address;
+//     }
+// }
+
+// class newProduct {
+//     constructor(name, category, price, discount, imgurl) {
+//         this.name = name;
+//         this.category = category;
+//         this.price = price;
+//         this.discount = discount;
+//         this.url = imgurl;
+//     }
+// }
+
+// class ourUser {
+//     constructor(email, pass) {
+//         this.email = email;
+//         this.pass = pass;
+//     }
+// }
+
+
+class newUser {
+    constructor(name,long,lat) {
+        this.name = name;
+        this.long = long;
+        this.lat = lat;
+        console.log("constructor done");
+    }
+}
---
+++ utilities.js
@@ -0,0 +1,16 @@
+  var config = {
+    apiKey: "AIzaSyBdvqqvkmlXQWW68Rf6QfBqSnEmRWUZcXA",
+    authDomain: "messaging-38981.firebaseapp.com",
+    databaseURL: "https://messaging-38981.firebaseio.com",
+    projectId: "messaging-38981",
+    storageBucket: "messaging-38981.appspot.com",
+    messagingSenderId: "750780048975"
+  };
+  firebase.initializeApp(config);
+
+
+  // function setCookie(name) {
+  //   console.log(name);
+  //   localStorage.setItem("username",name);
+  //   // localStorage.setId("userID",);
+  // }
